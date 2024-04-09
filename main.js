7503616cb89be46223137b759369600c1045a08d
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-04-09 20:49:17 -0300

    main

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const dataAtual = new Date();
 let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 2024-05-30");
 dataObjetivo = new Date(dataObjetivo+"T23:59:59");
+let diasQueFaltam = (dataObjetivo - dataAtual) / 86400000;
+let diasQueFaltam = Math.floor((dataObjetivo - dataAtual) / 86400000);
+
